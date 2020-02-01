7d10fa05e8838f07091f285d16508ed5116287d8
shubham860 <shubham.chauhan860@gmail.com>
2020-02-02 02:22:58 +0530

    commit-1

---
+++ todoApp/src/components/History.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+function History(props) {
+    return (
+        <View>
+            <Text>History</Text>
+        </View>
+    );
+}
+
+export default History;
